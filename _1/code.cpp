0811833633654af945176fe21cc40a03a60cfbad
svdanilova4315 <svdanilova@stud.etu.ru>
2025-09-25 03:37:08 +0300

    practice 4 : machine timer

--- _1/code.cpp
+++ _1/code.cpp
@@ -9,7 +9,7 @@ using namespace std;
 string universe = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯЁ";
 
 void init(set<string>& s) {
-    int size = rand() % 128; // -> <= 127 chars in set
+    int size = 22; // -> <= 127 chars in set
     int ind;
     for (int i = 0; i < size; i++) {
         ind = rand()%32+rand()%2;
@@ -25,6 +25,7 @@ void print(set<string>& s) {
 
 int main() {
     auto t1 = std::chrono::high_resolution_clock::now();
+    auto tt1 = clock();
 
     srand(time(0));
     setlocale(LC_ALL, "Russian");
@@ -65,7 +66,10 @@ int main() {
     cout << "Множество E = A ∩ B ∩ C - D: "; print(setE);
 
     auto t2 = std::chrono::high_resolution_clock::now();
+    auto tt2 = clock();
+
     cout << "Время выполнения: " << std::chrono::duration_cast<std::chrono::duration<double, micro>>(t2-t1).count() << " мкс" << endl;
+    cout << "Счетчик тиков: " << tt2-tt1 << endl;
 
     return 0;
 }
