1519c826fe5f999640f3e8317ef94f6f3cc82c1d
svdanilova4315 <danilova.sofia@gmail.com>
2025-10-08 20:27:08 +0300

    lab2 : edited code_list.cpp, need to compare algorithm time consumption in previous commit and now

--- _2/code_list.cpp
+++ _2/code_list.cpp
@@ -7,12 +7,12 @@
 
 using namespace std;
 
-void print(LinkedList<string>& l) {
+void print(LinkedList<string> &l) {
     for (auto ch : l) cout << ch << " ";
     cout << endl;
 }
 
-void input(LinkedList<string>& l) {
+void input(LinkedList<string> &l) {
     string line;
     getline(cin, line, '\n');
 
@@ -38,16 +38,16 @@ void input(LinkedList<string>& l) {
 int main() {
     setlocale(LC_ALL, "Russian");
     
-    LinkedList<string> listA;
-    LinkedList<string> listB;
-    LinkedList<string> listC;
-    LinkedList<string> listD;
-    LinkedList<string> listE;
+    LinkedList<string>* listA = new LinkedList<string>;
+    LinkedList<string>* listB = new LinkedList<string>;
+    LinkedList<string>* listC = new LinkedList<string>;
+    LinkedList<string>* listD = new LinkedList<string>;
+    LinkedList<string>* listE = new LinkedList<string>;
 
-    cout << "A: "; input(listA);
-    cout << "B: "; input(listB);
-    cout << "C: "; input(listC);
-    cout << "D: "; input(listD);
+    cout << "A: "; input(*listA);
+    cout << "B: "; input(*listB);
+    cout << "C: "; input(*listC);
+    cout << "D: "; input(*listD);
 
 
    auto t1 = std::chrono::high_resolution_clock::now();
@@ -55,36 +55,42 @@ int main() {
 
     cout << endl;
 
-    cout << "A: "; print(listA);
-    cout << "B: "; print(listB);
-    cout << "C: "; print(listC);
-    cout << "D: "; print(listD);
+    cout << "A: "; print(*listA);
+    cout << "B: "; print(*listB);
+    cout << "C: "; print(*listC);
+    cout << "D: "; print(*listD);
     
     // Находим A ∩ B ∩ C - D
     bool found = 0;
-    for (auto chA : listA) {
-        for (auto chB : listB) {
-            for (auto chC : listC) {
+    for (auto chA : *listA) {
+        for (auto chB : *listB) {
+            for (auto chC : *listC) {
                 if (chA == chB && chA == chC) {
-                    listE.push_back(chA); found = 1; break;
+                    (*listE).push_back(chA); found = 1; break;
                 }
             }
             if (found) {found = 0; break;}
         }
     }
 
-    for (auto itE = listE.begin(); itE != listE.end(); ++itE) {
-        for (auto itD = listD.begin(); itD != listD.end(); ++itD) {
-            if (*itE == *itD) {listE.remove(*itE); break;}
+    for (auto itE = (*listE).begin(); itE != (*listE).end(); ++itE) {
+        for (auto itD = (*listD).begin(); itD != (*listD).end(); ++itD) {
+            if (*itE == *itD) {(*listE).remove(*itE); break;}
         }
     }
     
 
     // Вывод результата
-    cout << "Множество E = A ∩ B ∩ C - D: "; print(listE);
+    cout << "Множество E = A ∩ B ∩ C - D: "; print((*listE));
 
     auto t2 = std::chrono::high_resolution_clock::now();
     auto tt2 = clock();
+
+    delete listA;
+    delete listB;
+    delete listC;
+    delete listD;
+    delete listE;
 
     cout << "Время выполнения: " << std::chrono::duration_cast<std::chrono::duration<double, micro>>(t2-t1).count() << " мкс" << endl;
     cout << "Счетчик тиков: " << tt2-tt1 << endl;
